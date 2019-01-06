4b0c9d2872294ac903b042b54c330e9b72614f7c
Vikram <belvikram@gmail.com>
2019-01-06 10:40:50 -0600

    object structure changed and handled corner clicks

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,91 @@ import Square from '../components/square';
 import Mine from '../components/mine';
 import Flag from '../components/flag';
 
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    boardState: boardState,//generateBoard(100),
+    isExploded: false
+  }
+
+  handleExpose(cellIndex){
+  const {boardState} = this.state
+  let bombs = 0
+  let cellsToCheck = []
+  if(cellIndex == 0 || cellIndex == boardSize-1 || cellIndex == boardSize*boardSize - boardSize || cellIndex == boardSize*boardSize - 1){
+    switch(cellIndex){
+      case 0: 
+        cellsToCheck = [1,boardSize,boardSize+1,]
+        break
+      case boardSize-1: 
+        cellsToCheck = cellsToCheck = [cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize] 
+        break
+      case boardSize*boardSize - boardSize: 
+       cellsToCheck = [cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1]  
+       break
+      case boardSize*boardSize - 1: 
+        cellsToCheck = [cellIndex - 1,cellIndex - (boardSize + 1),cellIndex- boardSize]
+        break
+    }
+  }else if(cellIndex%boardSize == boardSize - 1){
+    /* Right Edge */
+    console.log('CLICK ON RIGHT EDGE');
+    cellsToCheck = [cellIndex - (boardSize + 1),cellIndex- boardSize,cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize]
+  }else if(cellIndex%boardSize == 0){
+    /* Left Edge */
+    console.log('CLICK ON Left EDGE');
+    cellsToCheck = [cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1,cellIndex + boardSize,cellIndex + (boardSize  + 1)]
+  }else if(cellIndex < boardSize){
+    /* Top Edge */
+    console.log('CLICK ON Top EDGE');
+    cellsToCheck = [cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize,cellIndex + (boardSize  + 1),cellIndex + 1] 
+  }else if(cellIndex > (boardSize*boardSize) - boardSize && cellIndex < (boardSize*boardSize) - 1){
+    /* Bottom Edge */
+    console.log('Bottom Edge');
+    cellsToCheck = [cellIndex - 1,cellIndex - (boardSize + 1),cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1]
+  }
+
+  cellsToCheck.forEach(cellIndex=>{
+    console.log(`CHECKING CELL----------------- ${cellIndex}`);
+    if(boardState[cellIndex].holds == 'BOMB'){
+      console.log(`Bomb Found at ${cellIndex}`);
+      bombs++
+    }
+  })
+  console.log(`BOMBS FOUND, ${bombs}`);
+  }
+
+  render() {
+    const {boardState} = this.state
+    return (
+      <Layout title={`Minesweeper (active)`}>
+        <Desk boardSize={boardSize}>
+          {
+              boardState.map(cell => {
+                console.log(`Cell has ${cell.holds} rendering ${cell.id} `)
+                // console.log(`cell.holds === 'BLANK'`)
+                // console.log(`cell.holds === 'BOMB'`)
+                return(
+                  <Square key={cell.id} disabled={(cell.holds == 'BLANK' || cell.holds == 'BOMB') && cell.isExposed } onClick={()=>this.handleExpose(cell.id)}>
+                    {cell.holds === 'BOMB' && cell.isExposed && <Mine /> }
+                    {cell.holds === 'NUMBER' && cell.isExposed && cell.id }
+                    {/* <Mine /> */}
+                    {/* {i === 25 && <Flag />}
+                    {i === 77 ? '20' : ''} */}
+                  </Square>
+                )
+               })
+          }
+      </Desk>
+    </Layout>
+    )
+  }
+}
+
+
 const cellStates = ['BOMB','NUMBER','BLANK']
-const boardSize = 9
+const boardSize = 10
 function fillCell(){
   const randomCellState = Math.floor(Math.random() * 3)
   return cellStates[randomCellState]
@@ -20,9 +103,11 @@ function generateBoard(size){
   let row = []
   for(let i=0;i<rows;i++){
       boardStates.push({
-        i: i,
-        cellHas: cellStates[Math.floor(Math.random() * 3)],
-        shown: false
+        id: i,
+        holds: cellStates[Math.floor(Math.random() * 3)],
+        neighboringBombs: 0,
+        isExposed: false,
+        isFlagged: false,
       })
   }
   return boardStates
@@ -38,7 +123,7 @@ function generateBoard(size){
 //       row.push({
 //         i: i,
 //         j: j,
-//         cellHas: fillCell(),
+//         holds: fillCell(),
 //         shown: false
 //       })
 //     }
@@ -47,97 +132,116 @@ function generateBoard(size){
 //   }
 //   return boardState
 // }
-function findBomb(cellIndex){
-  console.log(cellIndex)
-  let bombs = 0
-  let cellsToCheck = []
-  if(cellIndex%boardSize == boardSize - 1){
-    /* Right Edge */
-    cellsToCheck = [cellIndex - (boardSize + 1),cellIndex- boardSize,cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize]
-  }else if(cellIndex%boardSize == 0){
-    /* Left Edge */
-    cellsToCheck = [cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1,cellIndex + boardSize,cellIndex + (boardSize  + 1)]
-  }else if(cellIndex < boardSize){
-    /* Top Edge */
-    cellsToCheck = [cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize,cellIndex + (boardSize  + 1),cellIndex + 1] 
-  }else if(cellIndex > (boardSize*boardSize) - boardSize && cellIndex < (boardSize*boardSize) - 1){
-    /* Bottom Edge */
-    console.log('Bttom Edge');
-    cellsToCheck = [cellIndex - 1,cellIndex - (boardSize + 1),cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1]
-  }
+// function findBomb(cellIndex){
+//   console.log(cellIndex)
+//   let bombs = 0
+//   let cellsToCheck = []
+//   if(cellIndex == 0 || cellIndex == boardSize-1 || cellIndex == boardSize*boardSize - boardSize || cellIndex == boardSize*boardSize - 1){
+//     console.log('Corner CLICKKKK');
+//     debugger
+//     switch(cellIndex){
+//       case 0: 
+//         cellsToCheck = [1,boardSize,boardSize+1,]
+//         break
+//       case boardSize-1: 
+//         cellsToCheck = [cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize]  
+//         break
+//       case boardSize*boardSize - boardSize: 
+//        cellsToCheck = []  
+//        break
+//       case boardSize*boardSize - 1: 
+//         cellsToCheck = []
+//         break
+//     }
+//   }else if(cellIndex%boardSize == boardSize - 1){
+//     /* Right Edge */
+//     console.log('CLICK ON RIGHT EDGE');
+//     cellsToCheck = [cellIndex - (boardSize + 1),cellIndex- boardSize,cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize]
+//   }else if(cellIndex%boardSize == 0){
+//     /* Left Edge */
+//     console.log('CLICK ON Left EDGE');
+//     cellsToCheck = [cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1,cellIndex + boardSize,cellIndex + (boardSize  + 1)]
+//   }else if(cellIndex < boardSize){
+//     /* Top Edge */
+//     console.log('CLICK ON Top EDGE');
+//     cellsToCheck = [cellIndex - 1,cellIndex +(boardSize- 1),cellIndex + boardSize,cellIndex + (boardSize  + 1),cellIndex + 1] 
+//   }else if(cellIndex > (boardSize*boardSize) - boardSize && cellIndex < (boardSize*boardSize) - 1){
+//     /* Bottom Edge */
+//     console.log('Bottom Edge');
+//     cellsToCheck = [cellIndex - 1,cellIndex - (boardSize + 1),cellIndex- boardSize,cellIndex-(boardSize - 1),cellIndex + 1]
+//   }
 
-  cellsToCheck.forEach(cellIndex=>{
-    console.log(`CHECKING CELL----------------- ${cellIndex}`);
-    if(boardState[cellIndex].cellHas == 'BOMB'){
-      console.log(`Bomb Found at ${cellIndex}`);
-      bombs++
-    }
-  })
+//   cellsToCheck.forEach(cellIndex=>{
+//     console.log(`CHECKING CELL----------------- ${cellIndex}`);
+//     if(boardState[cellIndex].holds == 'BOMB'){
+//       console.log(`Bomb Found at ${cellIndex}`);
+//       bombs++
+//     }
+//   })
 
-  console.log(`BOMBS FOUND, ${bombs}`);
-  // for(let i = cellIndex - (boardSize+1); i <= cellIndex - (boardSize-1);i++){
-  //   console.log(`CHECKING CELL----------------- ${i}`);
-  //   if(cellIndex%boardSize != boardSize - 1){
-  //     if(boardState[i].cellHas == 'BOMB'){
-  //       console.log(`Bomb Found at ${i}`);
-  //       bombs++
-  //     }
-  //   }else{
-  //     console.log(`CLICKED ON EDGE CELL`);
-  //   }
-  // }
-  // for(let i = cellIndex -1; i <= cellIndex +1; i++){
-  //   console.log(`CHECKING CELL----------------- ${i}`);
-  //   if(boardState[i].cellHas == 'BOMB' && i != cellIndex){
-  //     console.log(`Bomb Found at ${i}`);
-  //     bombs++
-  //   }
-  // }
-  // for(let i = cellIndex + (boardSize-1); i <= cellIndex + (boardSize+1);i++){
-  //   console.log(`CHECKING CELL----------------- ${i}`);
-  //   if(boardState[i].cellHas == 'BOMB'){
-  //     console.log(`Bomb Found at ${i}`);
-  //     bombs++
-  //   }
-  // }
-  // console.log(`BOMBS FOUND, ${bombs}`);
-}
-
-const Index = () => {
-//   const boardState = []
-//   for(let i=0;i<100;i++){
-//     boardState.push({
-//       i: i,
-//       cellHas: cellStates[Math.floor(Math.random() * 3)],
-//       shown: false
-//     })
+//   console.log(`BOMBS FOUND, ${bombs}`);
+//   // for(let i = cellIndex - (boardSize+1); i <= cellIndex - (boardSize-1);i++){
+//   //   console.log(`CHECKING CELL----------------- ${i}`);
+//   //   if(cellIndex%boardSize != boardSize - 1){
+//   //     if(boardState[i].holds == 'BOMB'){
+//   //       console.log(`Bomb Found at ${i}`);
+//   //       bombs++
+//   //     }
+//   //   }else{
+//   //     console.log(`CLICKED ON EDGE CELL`);
+//   //   }
+//   // }
+//   // for(let i = cellIndex -1; i <= cellIndex +1; i++){
+//   //   console.log(`CHECKING CELL----------------- ${i}`);
+//   //   if(boardState[i].holds == 'BOMB' && i != cellIndex){
+//   //     console.log(`Bomb Found at ${i}`);
+//   //     bombs++
+//   //   }
+//   // }
+//   // for(let i = cellIndex + (boardSize-1); i <= cellIndex + (boardSize+1);i++){
+//   //   console.log(`CHECKING CELL----------------- ${i}`);
+//   //   if(boardState[i].holds == 'BOMB'){
+//   //     console.log(`Bomb Found at ${i}`);
+//   //     bombs++
+//   //   }
+//   // }
+//   // console.log(`BOMBS FOUND, ${bombs}`);
 // }
-  return(
-    <Layout title={`Minesweeper (active)`}>
-    <Desk boardSize={boardSize}>
-      {
-        boardState.slice(0,(boardSize*boardSize)-100).map(cell => {
-          // console.log(`Cell has ${cell.cellHas} rendering ${cell.i} `)
-          // console.log(`cell.cellHas === 'BLANK'`)
-          // console.log(`cell.cellhas === 'BOMB'`)
-          return(
-            <Square key={cell.i} disabled={cell.cellHas == 'BLANK' || cell.cellhas == 'BOMB'|| cell.cellhas == 'NUMBER'} onClick={()=>findBomb(cell.i)}>
-              {cell.cellHas === 'BOMB' && <Mine /> }
-              {cell.cellHas === 'NUMBER' && cell.i }
-              {cell.cellHas === 'BOMB' && cell.i }
-              {/* <Mine /> */}
-              {/* {i === 25 && <Flag />}
-              {i === 77 ? '20' : ''} */}
-            </Square>
-          )
-      })
-    }
-    </Desk>
-  </Layout>
-  )
-  }
 
-export default Index;
+// const Index = () => {
+// //   const boardState = []
+// //   for(let i=0;i<100;i++){
+// //     boardState.push({
+// //       i: i,
+// //       holds: cellStates[Math.floor(Math.random() * 3)],
+// //       shown: false
+// //     })
+// // }
+//   return(
+//     <Layout title={`Minesweeper (active)`}>
+//     <Desk boardSize={boardSize}>
+//       {
+//         boardState.map(cell => {
+//           console.log(`Cell has ${cell.holds} rendering ${cell.id} `)
+//           // console.log(`cell.holds === 'BLANK'`)
+//           // console.log(`cell.holds === 'BOMB'`)
+//           return(
+//             <Square key={cell.id} disabled={cell.holds == 'BLANK' || cell.holds == 'BOMB' } onClick={()=>findBomb(cell.id)}>
+//               {cell.holds === 'BOMB' && <Mine /> }
+//               {cell.holds === 'NUMBER' && cell.id }
+//               {/* <Mine /> */}
+//               {/* {i === 25 && <Flag />}
+//               {i === 77 ? '20' : ''} */}
+//             </Square>
+//           )
+//       })
+//     }
+//     </Desk>
+//   </Layout>
+//   )
+//   }
+
+// export default Index;
 
 
 /* const Index = () => (
